4c793d58aac81b43414cb42076e65a78491bcc24
BT453567 <110046571+BT453567@users.noreply.github.com>
2024-04-17 14:04:12 +0100

    Add files via upload
    Removed making style changes from JS file and have used adding / removing from class list.

--- js/main.js
+++ js/main.js
@@ -6,35 +6,26 @@ const mobileSocialBar = document.getElementById("social-links-mobile");
 
 document.getElementById("share-icon-button").addEventListener("click", function() {
     if(window.innerWidth >= 481) {
-        if (socialBar.style.display == "none") {
-            socialBar.style.display = "flex";
-        } else {
-            socialBar.style.display = "none";  
-        }
+        socialBar.classList.toggle("hide");
     } else {
-        
-        if (avatarContainer.style.display == "none") {
-            avatarContainer.style.display = "block";
-            nameContainer.style.display = "block";
-            bottomContainer.classList.remove("dark-colour-background");
-            mobileSocialBar.style.display = "none";
-        } else {
-            avatarContainer.style.display = "none";
-            nameContainer.style.display = "none";
-            bottomContainer.classList.add("dark-colour-background");
-            mobileSocialBar.style.display = "flex";
-        }
+        avatarContainer.classList.toggle("hide");
+        nameContainer.classList.toggle("hide");
+        bottomContainer.classList.toggle("dark-colour-background");
+        mobileSocialBar.classList.toggle("hide");
     }
 });
 
 window.addEventListener('resize', function() {
     if (window.innerWidth <= 480) {
-        socialBar.style.display = "none";
+        socialBar.classList.add("hide");
     } else {
         bottomContainer.classList.remove("dark-colour-background");
-        avatarContainer.style.display = "block";
-        nameContainer.style.display = "block";
-        mobileSocialBar.style.display = "none";
-        
+        avatarContainer.classList.remove("hide");
+        nameContainer.classList.remove("hide");
+        mobileSocialBar.classList.add("hide");
     }
 });
+
+
+
+  
